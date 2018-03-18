cbd75845d0eeeb0ddaf8eafe280c55a15d27e6a0
John Rivers <jrivers@nmsu.edu>
2018-03-18 17:11:57 -0600

    Trying to fix DB connection.

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
 
 //defenitions go here
-
+     private $DB_HOST = 'dbclass.cs.nmsu.edu';
+     private $DB_USER = 'cs448sp18team4';
+     private $DB_PSW = 'JJ_F3b8gB0zsq87H';
+     private $DB_NAME = 'cs448sp18team4';
 
 function connectDB() {
     $link = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_NAME);
