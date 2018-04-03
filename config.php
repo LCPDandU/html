5ac1a90ea7987915a526c9ddfb7c5436c81af6a9
John Rivers <jrivers@nmsu.edu>
2018-04-02 23:06:49 -0600

    Added API key for push notifications.

--- config.php
+++ config.php
@@ -1,10 +1,12 @@
 <?php
 
-//defenitions go here
+//definitions go here
 define('DB_HOST', 'dbclass.cs.nmsu.edu'); /*Database Server*/
 define('DB_NAME', 'cs448sp18team4'); /*Database Name*/
 define('DB_USER', 'cs448sp18team4'); /*Database Username*/
 define('DB_PWD', 'JJ_F3b8gB0zsq87H'); /*Database Password*/
+
+ define('FIREBASE_API_KEY', 'AIzaSyCKZqpdli2qcVtTRA6TdIP3wW5CNjI7M4E'); /*Firebase API key for push notifications.*/
 
 function connectDB() {
     $link = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_NAME);
