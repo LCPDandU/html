0dff11edf94dc108a46bd0877bf6442307f56071
jmmuniz <36388166+jmmuniz@users.noreply.github.com>
2018-04-24 17:40:48 -0600

    passwords are hashed
    hashed passwords for: addUserSubmit, accountMangementMyAccountInfoSubmit
    password hash verify: loginSubmit

--- loginSubmit.php
+++ loginSubmit.php
@@ -33,83 +33,91 @@ elseif (strlen( $_POST['Password']) > 32 || strlen($_POST['Password']) < 7)
     $message = 'Incorrect Length for Password';
 }
 
-else{
-   $LoginID = filter_var($_POST['LoginID'], FILTER_SANITIZE_STRING);
-   $Password = filter_var($_POST['Password'], FILTER_SANITIZE_STRING);
+else
+{
+    // Store LoginID and Passwords as variables
+    $LoginID = filter_var($_POST['LoginID'], FILTER_SANITIZE_STRING);
+    $Password = filter_var($_POST['Password'], FILTER_SANITIZE_STRING);
 
-   // REST command url. http://localhost works for both a localhost
-   //  and when hosted on the server.
-   $url="http://localhost/public/api/users/login/LoginID/$LoginID/Password/$Password";
+    try
+    {
+         //Connect to MySQL Database  mysqli(Server,User,Password,Database)
+        $link = connectDB();
 
-   // Need to read the file created by the REST command.
-   $handle=fopen($url,"r");
-   // Only enter if handle variable was successfully created.
-   if($handle){
-      // Only enter if current line contains data.
-      if(($line=fgets($handle))!==false){
-         // Decode json response. If it is empty then login has failed.
-         $arr=json_decode($line,true);
-         // Output error if empty.
-         if(empty($arr)){
-            $message="Login Failed";
-         }
-         //If login hasn't failed, then global variables are set, a token is generated and assigned to the User
-         else foreach($arr as $row){
-               $userID = $row['ID'];
-               $userLoginID = $row['LoginID'];
-               $userName = $row['Name'];
 
-               // Set the session userID, LoginID, and Name.
-               $_SESSION['userID'] = $userID;
-               $_SESSION['LoginID'] = $userLoginID;
-               $_SESSION['Name'] = $userName;
+        // Prep SQL statement which will compare the user credentials with what is stored in the database
+        $sql = "SELECT * FROM User WHERE LoginID = '".$LoginID."' AND AccountStatus != 'Pending'";//AND Password = '".$Password."'";
+        //echo $sql."<br>";
 
-               // Success message.
-               $message = 'You are now logged in';
+        //Run the query
+        if($result=mysqli_query($link,$sql))
+        {
+          //give values to session variables
+          while($row = mysqli_fetch_assoc($result)) {
+            
+            //verify the password with the hash password
+            if(password_verify($Password, $row['Password']))
+            { 
+              $userID = $row['ID'];
+              $userLoginID = $row['LoginID']; 
+              $userName = $row['Name'];
 
-               // Generate key for this login session.
-               $token = bin2hex(random_bytes(64));
-               $_SESSION['token'] = $token;
+              // Set the session userID, LoginID, and Name
+              $_SESSION['userID'] = $userID;
+              $_SESSION['LoginID'] = $userLoginID;
+              $_SESSION['Name'] = $userName;
 
-               // Need to generate timestamp to allow token to expire.
+              $message = 'You are now logged in';
+            
+              // Generate key for this Login
+              $token = bin2hex(random_bytes(64));
+              $_SESSION['token'] = $token;
 
-               // Update the database by assigning the generated token to the current user.
-               $sql = "UPDATE User SET Token = :Token WHERE LoginID = :LoginID";
+              $sql = "UPDATE User SET Token = :Token WHERE LoginID = :LoginID";
 
-               // Use prepare, bindParam, and PDO statements to more securely
-               //  add the new token to the database.
-               try{
-                 // Get DB object
-                 $configDB = parse_ini_file('./db.ini');
-                 $db = new db($configDB['DB_HOST'],$configDB['DB_USER'],$configDB['DB_PWD'],$configDB['DB_NAME']);
-                 // Call connect; connect to database.
-                 $db = $db->connect();
+              try{
+                // Get DB object
+                $db = new db();
+                // Call connect; connect to database.
+                $db = $db->connect();
 
-                 // PHP Data Objects (PDO) statements.
-                 $stmt = $db->prepare($sql);
+                # PDO statement
+                $stmt = $db->prepare($sql);
 
-                 // Bind variables and parameters.
-                 $stmt->bindParam(':LoginID', $userLoginID);
-                 $stmt->bindParam(':Token', $token);
+                $stmt->bindParam(':LoginID', $userLoginID);
+                $stmt->bindParam(':Token', $token);
 
-                 // Execute the SQL statement.
-                 $stmt->execute();
-                 echo '{"notice": {"text": "Token Added"}';
+                $stmt->execute();
+                echo '{"notice": {"text": "Token Added"}';
 
-               }//end try
-               catch(PDOException $e){
-                 echo '{"error": {"text": '.$e->getMessage().'}';
-               }//end catch
+              } 
+              catch(PDOException $e){
+                echo '{"error": {"text": '.$e->getMessage().'}';
+              }
 
-               echo ("<script>
-                  window.location.assign('home');
-                  </script>");
+              echo ("<script>
+                       window.location.assign('home');
+                     </script>");
 
-               exit();
-         }//end foreach
-      }//end if
-   }//end if
-}//end else
+              exit();
+            
+            }//end password verify
+            else
+            {
+              $message = 'Invalid password';
+            }
+            
+          }//end while
+        }
+        if($userID == false) {
+            $message = 'Login Failed';
+        }
+    }
+    catch(Exception $e)
+    {
+        $message = 'Unable to process request';
+    }
+}
 
 ?>
 
@@ -121,8 +129,4 @@ else{
     <body>
     <p><?php echo $message; ?>
     </body>
-
-    <form action="index.php" method="Post">
-      <input type="submit" value="Submit"/>
-    </form>
 </html>
