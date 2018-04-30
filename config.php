0417c3867594e4b9675e2c57e58373aea785589d
Christopher Scirone <cscirone2@hotmail.com>
2018-04-29 23:32:03 -0600

    config.php change
    Small change to config.php. Simply reverted to old DB connection format to test on the server.

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 // define firebase api key
 $FBconfig = parse_ini_file('../db.ini');
 define('FIREBASE_API_KEY', $FBconfig['FIREBASE_API_KEY']);
@@ -12,7 +12,25 @@ function connectDB() {
   }
   return $con;
 }
+*/
 
-define('URL_START', 'http://localhost');
+$config=parse_ini_file('../db.ini');
+define('DB_HOST', $config['DB_HOST']);
+define('DB_NAME', $config['DB_NAME']);
+define('DB_USER', $config['DB_USER']);
+define('DB_PWD', $config['DB_PWD']);
+
+define('FIREBASE_API_KEY', $config['FIREBASE_API_KEY']);
+
+function connectDB(){
+   $link=new mysqli(DB_HOST,DB_USER,DB_PWD,DB_NAME);
+   if($link->connect_error){
+      die("Connection Failed: " . $link->connect_error);
+   }
+   return $link;
+}
+
+//define('URL_START', 'http://localhost');
+define('URL_START', 'https://tm4sp18.cs.nmsu.edu');
 
 ?>
